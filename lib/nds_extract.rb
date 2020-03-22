f95f2003d0e8e96f2b4de299adfee4c6b909c638
George Chen <georgechen1338@gmail.com>
2020-03-22 18:56:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -53,6 +53,20 @@ def total_gross(source)
     director_index += 1
   end
   grand_total
+  
+  dir_to_earnings_hash = directors_totals(source)
+  dir_names = list_of_directors(source)
+  i = 0
+
+  total = 0
+
+  while i < dir_names.length do
+    dir_name = dir_names[i]
+    total += dir_to_earnings_hash[dir_name]
+    i += 1
+  end
+
+  total
 end
 
 
