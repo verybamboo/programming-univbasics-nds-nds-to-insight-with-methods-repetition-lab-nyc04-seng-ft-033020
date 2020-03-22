239f881c1ef11fe8cee24423f88f10f6996d8ec7
George Chen <georgechen1338@gmail.com>
2020-03-22 18:58:54 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,17 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
 
-def directors_totals(source)
-  result = {}
-  director_index = 0
-  while director_index < source.size do
-    director = source[director_index]
-    result[director[:name]] = gross_for_director(director)
-    director_index += 1
-  end
-  result
-end
-
 def gross_for_director(d)
   total = 0
   index = 0
@@ -24,8 +13,26 @@ def gross_for_director(d)
   total
 end
 
+def directors_totals(source)
+  result = {}
+  director_index = 0
+  while director_index < source.size do
+    director = source[director_index]
+    result[director[:name]] = gross_for_director(director)
+    director_index += 1
+  end
+  result
+end
+
 def list_of_directors(source)
   # Write this implementation
+  director_index = 0
+  dir_arr = []
+  while director_index < source.length do
+    dir_arr << source[director_index][:name]
+    director_index += 1
+  end
+  p dir_arr
 end
 
 def total_gross(source)
@@ -38,6 +45,21 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  
+  dir_to_earnings_hash = directors_totals(source)
+  dir_names = list_of_directors(source)
+  
+  index = 0
+  grand_total = 0
+
+  while index < dir_names.length do
+    dir_name = dir_names[index]
+    grand_total += dir_to_earnings_hash[dir_name]
+    index += 1
+  end
+
+  grand_total
 end
+
 
 
